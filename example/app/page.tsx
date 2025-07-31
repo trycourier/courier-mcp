f6ed28ffd9c70db079a1996e5f4f6c56a65a2e10
Mike Miller <mike@mikemiller.design>
2025-07-31 10:33:54 -0700

    Example not a submodule

---
+++ example/app/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useEffect } from 'react';
+import { CourierInbox, useCourier } from '@trycourier/courier-react';
+
+export default function App() {
+  const courier = useCourier();
+
+  useEffect(() => {
+    courier.shared.signIn({
+      userId: process.env.NEXT_PUBLIC_USER_ID!,
+      jwt: process.env.NEXT_PUBLIC_JWT!,
+    });
+  }, []);
+
+  return <CourierInbox />;
+}
