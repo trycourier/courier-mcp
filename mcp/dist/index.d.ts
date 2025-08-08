e17b908ff10eef38d6d7931ed559608f95b8d237
Mike Miller <mike@mikemiller.design>
2025-08-08 15:07:54 -0700

    Dist drop

--- mcp/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
-import { CourierClient } from './client/courier-client.js';
-export default class CourierMcp extends McpServer {
-    readonly client: CourierClient;
-    constructor(headers?: Record<string, any>);
-    private registerTools;
-}
